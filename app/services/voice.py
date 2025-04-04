3e21495b71463a0b39e0e02a3064261e4f29555d
andrew001s <andresroman45678@gmail.com>
2025-04-03 21:56:55 -0500

    feat(voice): implementar streaming de audio utilizando WebSocket

--- app/services/voice.py
+++ app/services/voice.py
@@ -1,24 +1,33 @@
-from fish_audio_sdk import Session, TTSRequest
+from fish_audio_sdk import WebSocketSession, TTSRequest
 from app.core.config import config
 from pydub import AudioSegment
 import io
 import simpleaudio as sa
 
-session = Session(config.FISH_API_KEY)
+ws_session = WebSocketSession(config.FISH_API_KEY)
+
+def stream(text):
+    for line in text.split():
+        yield line + " "
 
 def play_audio(response: str):
-    response_stream = session.tts(TTSRequest(
+    tts_request = TTSRequest(
+        text="",
         reference_id=config.ID_VOICE,
-        text=response,
-        prosody={
-            "volume": 0.5,
-        }
-        
-    ))
+    )
+
     try:
-        audio_data = b''.join(response_stream)
-        audio = AudioSegment.from_mp3(io.BytesIO(audio_data))
-        play_obj = sa.play_buffer(audio.raw_data, num_channels=audio.channels, bytes_per_sample=audio.sample_width, sample_rate=audio.frame_rate)
+        audio_buffer = io.BytesIO()
+
+        for chunk in ws_session.tts(tts_request, stream(response), backend="speech-1.5"):
+            audio_buffer.write(chunk)
+
+        audio_buffer.seek(0)
+        audio = AudioSegment.from_mp3(audio_buffer)
+
+        play_obj = sa.play_buffer(audio.raw_data, num_channels=audio.channels,
+                                  bytes_per_sample=audio.sample_width, sample_rate=audio.frame_rate)
         play_obj.wait_done()
+
     except Exception as e:
-        print(e)
+        print("Error:", e)
