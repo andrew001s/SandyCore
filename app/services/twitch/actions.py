9afd88ddf9c7930562a2c2755fb2aa2bee2ff87f
andrew001s <andresroman45678@gmail.com>
2025-04-07 20:33:20 -0500

    refactor: ♻️ eliminar impresión de ID de juego en la función de cambio de juego

--- app/services/twitch/actions.py
+++ app/services/twitch/actions.py
@@ -21,7 +21,6 @@ async def change_game(game: str):
    async for g in twitch_instance.get_games(names=[game]):
       game_id = g.id
       break 
-   print(game_id)
    await twitch_instance.modify_channel_information(user_id, game_id)
    await twitch_instance.send_chat_message(
       broadcaster_id=user_id,
