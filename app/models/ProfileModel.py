c9887760692f40d97b19022e4df27e7f7cf6c437
andrew001s <andresroman45678@gmail.com>
2025-05-07 22:46:52 -0500

    feat: ✨ agregar endpoint para obtener el perfil de usuario

---
+++ app/models/ProfileModel.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class ProfileModel(BaseModel):
+    id: int
+    username: str
+    email: str
+    picProfile: str
--- app/services/twitch/twitch.py
+++ app/services/twitch/twitch.py
@@ -1,12 +1,24 @@
 import app.services.twitch.auth.auth as auth
 from app.services.twitch.chat.chat_handler import setup_chat
 from app.services.twitch.events.eventsub_handler import setup_eventsub
+from app.models.ProfileModel import ProfileModel
+
 
 async def run_bot(bot: bool = False):
-    twitch,twitch_bot, user_id = await auth.create_twitch_instance(bot)
-    if bot:    
+    twitch, twitch_bot, user_id = await auth.create_twitch_instance(bot)
+    if bot:
         await setup_chat(twitch_bot)
     else:
         await setup_chat(twitch)
     await setup_eventsub(twitch, user_id)
 
+
+async def get_user_profile() -> dict:
+    user = auth.user
+    profile = ProfileModel(
+        id=user.id,
+        username=user.display_name,
+        email=user.email,
+        picProfile=user.profile_image_url,
+    )
+    return profile.model_dump() 
